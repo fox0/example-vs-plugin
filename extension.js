76a5244e6ba8ea57bbeba35e23b8cf3931b42614
fox0 <red-fox0@yandex.ru>
2022-02-22 22:45:27 +0700

    +registerCompletionItemProvider(

--- extension.js
+++ extension.js
@@ -1,35 +1,89 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+class CIP1 {
+    /**
+     * @param {vscode.TextDocument} document
+     * @param {vscode.Position} position
+     * @param {vscode.CancellationToken} token
+     * @param {vscode.CompletionContext} context
+     * @returns {vscode.CompletionItem[]}
+     */
+    provideCompletionItems(document, position, token, context) {
+        // a simple completion item which inserts `Hello World!`
+        const simpleCompletion = new vscode.CompletionItem('Hello World!');
+
+        // a completion item that inserts its text as snippet,
+        // the `insertText`-property is a `SnippetString` which will be
+        // honored by the editor.
+        const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+        snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+        snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
+
+        // a completion item that can be accepted by a commit character,
+        // the `commitCharacters`-property is set which means that the completion will
+        // be inserted and then the character will be typed.
+        const commitCharacterCompletion = new vscode.CompletionItem('console');
+        commitCharacterCompletion.commitCharacters = ['.'];
+        commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
+
+        // a completion item that retriggers IntelliSense when being accepted,
+        // the `command`-property is set which the editor will execute after 
+        // completion has been inserted. Also, the `insertText` is set so that 
+        // a space is inserted after `new`
+        const commandCompletion = new vscode.CompletionItem('new');
+        commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+        commandCompletion.insertText = 'new ';
+        commandCompletion.command = {
+            command: 'editor.action.triggerSuggest',
+            title: 'Re-trigger completions...'
+        };
+
+        // return all completion items as array
+        return [
+            simpleCompletion,
+            snippetCompletion,
+            commitCharacterCompletion,
+            commandCompletion
+        ];
+    }
+}
+
+class CIP2 {
+    /**
+     * @param {vscode.TextDocument} document
+     * @param {vscode.Position} position
+     * @param {vscode.CancellationToken} token
+     * @param {vscode.CompletionContext} context
+     * @returns {vscode.CompletionItem[]}
+     */
+    provideCompletionItems(document, position, token, context) {
+        // get all text until the `position` and check if it reads `console.`
+        // and if so then complete if `log`, `warn`, and `error`
+        const linePrefix = document.lineAt(position).text.substr(0, position.character);
+        if (!linePrefix.endsWith('console.')) {
+            return undefined;
+        }
+
+        return [
+            new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
+            new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
+        ];
+    }
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions = [
+        vscode.languages.registerCompletionItemProvider('plaintext', new CIP1()),
+        vscode.languages.registerCompletionItemProvider('plaintext', new CIP2(), '.')
+    ];
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
-// eslint-disable-next-line no-undef
 module.exports = {
     activate,
     deactivate
